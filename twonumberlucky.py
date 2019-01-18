9828ddcfd13aa024a8a1b18b92930f4b472b0678
NTQHoan <ngohoan1993@gmail.com>
2019-01-18 12:15:04 +0700

    fix pep8 file twonumberlucky.py

--- twonumberlucky.py
+++ twonumberlucky.py
@@ -1,15 +1,25 @@
+__doc__ = '''
+Requirement:
+-   Check the lotto result
+    -   Case with arguments, check by 2 last digits
+-   run command under the following pattern:
+        ketqua.py [NUMBER1] [NUMBER2] [...]
+'''
+
 import argparse
-import json
 import requests
 from bs4 import BeautifulSoup as BS
 
 url = 'http://ketqua.net'
 
+
 def get_number():
     req = requests.get(url)
-    lucky_number = BS(req.text, 'html.parser').find('tbody').get_text(' ').split()
+    lucky_number = BS(req.text, 'html.parser').find(
+        'tbody').get_text(' ').split()
     number_of_lucky = [number for number in lucky_number if number.isdigit()]
     return number_of_lucky
+
 
 def check_number(numbers):
     prizes = get_number()
@@ -18,15 +28,24 @@ def check_number(numbers):
         if number in lo_numbers:
             count = lo_numbers.count(number)
             if count >= 1:
-                print("number lucky: {} ,number of time: {}".format(str(number), count))
+                print(
+                    "number lucky: {} ,number of time: {}".format(
+                        str(number), count))
             else:
                 print('Good luck next time:(')
 
+
 def arg_parse():
     parser = argparse.ArgumentParser(description='Check Jackpot: )')
-    parser.add_argument('nb', type=int, nargs='*', help='an integer for the check')
+    parser.add_argument(
+        'nb',
+        type=int,
+        nargs='*',
+        help='an integer for the check')
     args = parser.parse_args()
     return args
+
+
 def main():
     input_data = arg_parse().nb
     check_number(input_data)
